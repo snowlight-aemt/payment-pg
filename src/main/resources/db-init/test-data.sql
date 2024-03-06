010bab10e0828f6c83b70a25167d256d17cb4042
snowlight-aemt <it.s.aemt@gmail.com>
2024-03-06 22:38:56 +0900

    FEAT: DB Test Data 추가

---
+++ src/main/resources/db-init/test-data.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO TB_PRODUCT(id, name, price, created_at, updated_at) VALUES
+(1, 'apple', 10000, now(), now()),
+(2, 'banana', 12000, now(), now()),
+(3, 'mango', 7000, now(), now()),
+(4, 'orange', 10000, now(), now());
