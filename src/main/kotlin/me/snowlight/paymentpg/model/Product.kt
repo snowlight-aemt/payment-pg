27ed6ad678ba3f492aa4b8b3b5fb496700bdacc0
snowlight-aemt <it.s.aemt@gmail.com>
2024-03-06 22:07:33 +0900

    Issue: Insert 가 되지 않은 이슈 - r2dbc
    - 생성할 Entity 가 존재하지 않는데, 해당 Insert 하지 않고 Update 하는 이슈가 존재.

--- src/main/kotlin/me/snowlight/paymentpg/model/Product.kt
+++ src/main/kotlin/me/snowlight/paymentpg/model/Product.kt
@@ -3,7 +3,10 @@ package me.snowlight.paymentpg.model
 import au.com.console.kassava.kotlinEquals
 import au.com.console.kassava.kotlinHashCode
 import au.com.console.kassava.kotlinToString
+import com.fasterxml.jackson.annotation.JsonIgnore
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.annotation.Id
+import org.springframework.data.domain.Persistable
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("TB_PRODUCT")
@@ -12,19 +15,9 @@ class Product (
     var id: Long = 0,
     var name: String = "",
     var price: Long = 0,
-): BaseEntity() {
-    override fun equals(other: Any?): Boolean {
-        return kotlinEquals(other, arrayOf(
-            Product::id,
-        ))
-    }
-
-    override fun hashCode(): Int {
-        return kotlinHashCode(arrayOf(
-            Product::id
-        ))
-    }
-
+): BaseEntity(), Persistable<Long> {
+    override fun equals(other: Any?) = kotlinEquals(other, arrayOf(Product::id))
+    override fun hashCode() = kotlinHashCode(arrayOf(Product::id))
     override fun toString(): String {
         return kotlinToString(arrayOf(
             Product::id,
@@ -32,6 +25,11 @@ class Product (
             Product::price,
         ), superToString = { super.toString() })
     }
+    override fun getId() = id
 
+    @Value("null")
+    @JsonIgnore
+    var new: Boolean = false
 
+    override fun isNew() = new
 }
--- src/test/kotlin/me/snowlight/paymentpg/PaymentPgApplicationTests.kt
+++ src/test/kotlin/me/snowlight/paymentpg/PaymentPgApplicationTests.kt
@@ -4,25 +4,22 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import me.snowlight.paymentpg.model.Product
 import me.snowlight.paymentpg.model.ProductRepository
-import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PaymentPgApplicationTests(
     @Autowired private val productRepository: ProductRepository,
 ): StringSpec({
     "product save" {
         val prevCount = productRepository.count()
-        val product = productRepository.save(Product(id = 1, name = "test 01", price = 100000))
+        productRepository.save(Product(id = 1, name = "test 01", price = 100000).apply { new = true })
         val currCount = productRepository.count()
 
-        prevCount shouldBe currCount + 1
+
+        currCount shouldBe prevCount + 1
     }
 }) {
-
-    @Test
-    fun contextLoads() {
-    }
-
 }
