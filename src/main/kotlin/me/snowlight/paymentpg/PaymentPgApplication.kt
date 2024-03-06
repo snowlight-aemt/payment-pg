761bbc5e1455ee804e9afe5cec31c1e456e89101
snowlight-aemt <it.s.aemt@gmail.com>
2024-03-06 22:29:17 +0900

    FEAT: Model Test Code

--- src/main/kotlin/me/snowlight/paymentpg/PaymentPgApplication.kt
+++ src/main/kotlin/me/snowlight/paymentpg/PaymentPgApplication.kt
@@ -2,8 +2,10 @@ package me.snowlight.paymentpg
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 
 @SpringBootApplication
+@EnableR2dbcAuditing
 class PaymentPgApplication
 
 fun main(args: Array<String>) {
--- src/test/kotlin/me/snowlight/paymentpg/PaymentPgApplicationTests.kt
+++ src/test/kotlin/me/snowlight/paymentpg/PaymentPgApplicationTests.kt
@@ -2,7 +2,11 @@ package me.snowlight.paymentpg
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import me.snowlight.paymentpg.model.Order
+import me.snowlight.paymentpg.model.OrderRepository
 import me.snowlight.paymentpg.model.Product
+import me.snowlight.paymentpg.model.ProductInOrder
+import me.snowlight.paymentpg.model.ProductInOrderRepository
 import me.snowlight.paymentpg.model.ProductRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -12,14 +16,30 @@ import org.springframework.test.context.ActiveProfiles
 @ActiveProfiles("test")
 class PaymentPgApplicationTests(
     @Autowired private val productRepository: ProductRepository,
+    @Autowired private val orderRepository: OrderRepository,
+    @Autowired private val productInOrderRepository: ProductInOrderRepository,
 ): StringSpec({
     "product save" {
         val prevCount = productRepository.count()
         productRepository.save(Product(id = 1, name = "test 01", price = 100000).apply { new = true })
         val currCount = productRepository.count()
 
+        currCount shouldBe prevCount + 1
+    }
+
+    "order save" {
+        val prevCount = orderRepository.count()
+        orderRepository.save(Order(userId = 1))
+        val currCount = orderRepository.count()
 
         currCount shouldBe prevCount + 1
     }
-}) {
-}
+
+    "productInOrder save" {
+        val prevCount = productInOrderRepository.count()
+        productInOrderRepository.save(ProductInOrder(1, 1, 1, 1))
+        val currCount = productInOrderRepository.count()
+
+        currCount shouldBe prevCount + 1
+    }
+})
